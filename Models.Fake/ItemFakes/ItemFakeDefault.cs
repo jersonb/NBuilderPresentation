b6989e5b12e91d9ec010b76194be2d22dd923350
Jerson Brito <jerson.silva@ambevtech.com.br>
2021-08-14 20:33:53 -0300

    updating

--- Models.Fake/ItemFakes/ItemFakeDefault.cs
+++ Models.Fake/ItemFakes/ItemFakeDefault.cs
@@ -4,15 +4,18 @@ namespace Models.Fake.ItemFakes
 {
     public class ItemFakeDefault : ModelFake<Item>
     {
+        protected override ISingleObjectBuilder<Item> SetBuilder()
+            => base.SetBuilder()
+                   .With(item => item.IsActive, true);
+
         protected override IListBuilder<Item> SetListBuild(int size)
             => base.SetListBuild(size)
-                    .All()
-                    .All()
-                    .Do(item => 
-                    {
-                        item.Name = Random.Phrase(10);
-                        item.Value = Random.Next(1m, 100m);
-                    })
-                    .With(item => item.IsActive, true);
+                   .All()
+                   .Do(item =>
+                   {
+                       item.Name = Random.Phrase(10);
+                       item.Value = Random.Next(1m, 100m);
+                   })
+                   .With(item => item.IsActive, true);
     }
 }
